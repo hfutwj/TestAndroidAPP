8aa14f12182fd93abbbad626259fca9dd01fe4b9
JasonWu <xcwujian@126.com>
2018-02-09 22:21:34 +0800

    First commit

---
+++ app/src/main/java/com/jason/testapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.jason.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text);
+        button =  (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("被按下了！");
+            }
+        });
+    }
+}
